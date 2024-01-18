0f86f8cf8352bbfa396da1ecf7ecdba86b048513
Ben <bjporter123@gmail.com>
2024-01-18 18:52:52 +0000

    setting up

--- src/App.js
+++ src/App.js
@@ -2,14 +2,5 @@ import React from "react";
 import { supabase } from "./lib/supabaseClient";
 
 export default function App() {
-  const login = async () => {
-    await supabase.auth.signInWithOAuth({
-      provider: "github",
-    });
-  };
-  return (
-    <div>
-      <button>Login with github</button>
-    </div>
-  );
+  return <div className="app"></div>;
 }
