a249d2eb4809b3e555a8375c2782506c1420cdef
Ben <bjporter123@gmail.com>
2024-01-25 13:49:09 +0000

    nav and search added

--- src/Theme.js
+++ src/Theme.js
@@ -1,7 +1,7 @@
-import { createContext, useState, useNemo, useMemo } from "react";
+import { createContext, useState, useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
 
-//colour design tokens
+// color design tokens export
 export const tokens = (mode) => ({
   ...(mode === "dark"
     ? {
@@ -16,12 +16,11 @@ export const tokens = (mode) => ({
           800: "#292929",
           900: "#141414",
         },
-
         primary: {
           100: "#d0d1d5",
           200: "#a1a4ab",
           300: "#727681",
-          400: "#434957",
+          400: "#1F2A40",
           500: "#141b2d",
           600: "#101624",
           700: "#0c101b",
@@ -74,14 +73,13 @@ export const tokens = (mode) => ({
           800: "#c2c2c2",
           900: "#e0e0e0",
         },
-
         primary: {
           100: "#040509",
           200: "#080b12",
           300: "#0c101b",
-          400: "#f2f0f0",
+          400: "#f2f0f0", // manually changed
           500: "#141b2d",
-          600: "#434957",
+          600: "#1F2A40",
           700: "#727681",
           800: "#a1a4ab",
           900: "#d0d1d5",
@@ -121,95 +119,96 @@ export const tokens = (mode) => ({
         },
       }),
 });
-//mui theme settings
+
+// mui theme settings
 export const themeSettings = (mode) => {
-  const colours = tokens(mode);
+  const colors = tokens(mode);
   return {
     palette: {
       mode: mode,
-      ...mode(
-        mode === "dark"
-          ? {
-              primary: {
-                main: colours.primary[500],
-              },
-              secondary: {
-                main: colours.greenAccent[500],
-              },
-              neutral: {
-                dark: colours.grey[700],
-                main: colours.grey[500],
-                light: colours.grey[100],
-              },
-              background: {
-                default: colours.primary[500],
-              },
-            }
-          : {
-              primary: {
-                main: colours.primary[100],
-              },
-              secondary: {
-                main: colours.greenAccent[500],
-              },
-              neutral: {
-                dark: colours.grey[700],
-                main: colours.grey[500],
-                light: colours.grey[100],
-              },
-              background: {
-                default: "#fcfcfc",
-              },
-            }
-      ),
+      ...(mode === "dark"
+        ? {
+            // palette values for dark mode
+            primary: {
+              main: colors.primary[500],
+            },
+            secondary: {
+              main: colors.greenAccent[500],
+            },
+            neutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100],
+            },
+            background: {
+              default: colors.primary[500],
+            },
+          }
+        : {
+            // palette values for light mode
+            primary: {
+              main: colors.primary[100],
+            },
+            secondary: {
+              main: colors.greenAccent[500],
+            },
+            neutral: {
+              dark: colors.grey[700],
+              main: colors.grey[500],
+              light: colors.grey[100],
+            },
+            background: {
+              default: "#fcfcfc",
+            },
+          }),
     },
     typography: {
-      fontFamily: ["Source Code Pro", "sans-serfif"].join(","),
+      fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
       fontSize: 12,
       h1: {
-        fontFamily: ["Source Code Pro", "sans-serfif"].join(","),
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
         fontSize: 40,
       },
       h2: {
-        fontFamily: ["Source Code Pro", "sans-serfif"].join(","),
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
         fontSize: 32,
       },
       h3: {
-        fontFamily: ["Source Code Pro", "sans-serfif"].join(","),
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
         fontSize: 24,
       },
       h4: {
-        fontFamily: ["Source Code Pro", "sans-serfif"].join(","),
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
         fontSize: 20,
       },
       h5: {
-        fontFamily: ["Source Code Pro", "sans-serfif"].join(","),
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
         fontSize: 16,
       },
       h6: {
-        fontFamily: ["Source Code Pro", "sans-serfif"].join(","),
+        fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
         fontSize: 14,
       },
     },
   };
 };
 
-//context for colour mode
-
-export const ColourModeContext = createContext({
-  toggleColourMode: () => {},
+// context for color mode
+export const ColorModeContext = createContext({
+  toggleColorMode: () => {},
 });
 
 export const useMode = () => {
   const [mode, setMode] = useState("dark");
 
-  const colourMode = useMemo(
+  const colorMode = useMemo(
     () => ({
-      toggleColourMode: () =>
+      toggleColorMode: () =>
         setMode((prev) => (prev === "light" ? "dark" : "light")),
     }),
     []
   );
+
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-  return [theme, colourMode]
+  return [theme, colorMode];
 };
--- src/scenes/global/SideBar.jsx
+++ src/scenes/global/SideBar.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { ProSideBar } from "react-pro-side";
+
 const SideBar = () => {
   return <div>SideBar</div>;
 };
